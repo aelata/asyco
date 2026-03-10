8ba75dfbda69123ef6dc5fc23ba6f19e1461629b
aelata <185669306+aelata@users.noreply.github.com>
2026-03-10 14:41:40 +0900

    CVE mitigation

--- .crossnote/parser.js
+++ .crossnote/parser.js
@@ -3,12 +3,21 @@
   // https://shd101wyy.github.io/markdown-preview-enhanced/#/extend-parser
 
   onWillParseMarkdown: async function(markdown) {
+    // rewrite .md to .html
     markdown = markdown.replace(
-     /\[([^\]]+)\]\(([^)]+).md(#.*)?\)/g, "[$1]($2.html$3)");
-     return markdown;
+      /\[([^\]]+)\]\(([^)]+).md(#.*)?\)/g, "[$1]($2.html$3)");
+    return markdown;
   },
 
   onDidParseMarkdown: async function(html) {
+    // for CVE-2025-65716
+    const re = new RegExp(
+      '<iframe\\b[^>]*(?:"[^"]*")*[^>]*\\bsandbox\ *=\ *"' +
+        '(?=[^"]*\\ballow-scripts\\b)' +
+        '(?=[^"]*\\ballow-same-origin\\b)' +
+        '.*?".*?>.*?</iframe>',
+      "gis"); // Global, IgnoreCase, DotAll
+    html = html.replace(re, " !!! IFRAME REMOVED !!! ");
     return html;
   },
 })
